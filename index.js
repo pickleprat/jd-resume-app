39a7e5b01255add68cfd33db540d0cc70674442a
pickleprat <prat16here@gmail.com>
2025-02-25 21:33:30 +0530

    added nodemodules and writing app in ts

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-console.log(document.getElementsByClassName("item-header-row"));
+function isValidURL(url) {
+    try {
+        new URL(url);
+        return true;
+    }
+    catch (_) {
+        return false;
+    }
+}
+function wrapTextInHTML(url, textContent) {
+    return isValidURL(url) && textContent ? "<a href=\"".concat(url, "\">").concat(textContent, "</a>") : "";
+}
+function insertContactInfo() {
+    var huggingfaceUrl = document.getElementById("huggingfaceUrl").value;
+    var githubUrl = document.getElementById("githubUrl").value;
+    var linkedinUrl = document.getElementById("linkedinUrl").value;
+    var mediumUrl = document.getElementById("mediumUrl").value;
+    var phoneNumber = document.getElementById("phoneNumber").value;
+    var tagContent = "India | ";
+    if (phoneNumber) {
+        tagContent += phoneNumber + " | ";
+    }
+    if (mediumUrl) {
+        tagContent += wrapTextInHTML(mediumUrl, "Medium") + " | ";
+    }
+    if (linkedinUrl) {
+        tagContent += wrapTextInHTML(linkedinUrl, "Linkedin") + " | ";
+    }
+    if (githubUrl) {
+        tagContent += wrapTextInHTML(githubUrl, "Github") + " | ";
+    }
+    if (huggingfaceUrl) {
+        tagContent += wrapTextInHTML(huggingfaceUrl, "Huggingface");
+    }
+    document.getElementById("contact-info").innerHTML = tagContent;
+    console.log(tagContent);
+}
