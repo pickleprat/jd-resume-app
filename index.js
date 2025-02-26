2883bf33a36a8edc5204767f579f2fec7685e767
pickleprat <prat16here@gmail.com>
2025-02-26 09:10:06 +0530

    Added gemini api keys and nearly completed section modifiers

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function isValidURL(url) {
 function wrapTextInHTML(url, textContent) {
     return isValidURL(url) && textContent ? "<a href=\"".concat(url, "\">").concat(textContent, "</a>") : "";
 }
-function insertContactInfo() {
+function addLinksToResume() {
     var huggingfaceUrl = document.getElementById("huggingfaceUrl").value;
     var githubUrl = document.getElementById("githubUrl").value;
     var linkedinUrl = document.getElementById("linkedinUrl").value;
@@ -33,5 +33,22 @@ function insertContactInfo() {
         tagContent += wrapTextInHTML(huggingfaceUrl, "Huggingface");
     }
     document.getElementById("contact-info").innerHTML = tagContent;
-    console.log(tagContent);
+}
+function modifySectionsInResume(jobDescription, sectionContent) {
+    var apiKey = document.getElementById("api-key").value;
+    var geminiApiUrl = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=".concat(apiKey);
+    var prompt = "\n    You are a ATS-conqueror bot. Your goal is to modify a list of texts to beat the ATS algorithm and secure a good score. \n    You will do this by using the a job description and then modifying the text I provide you with in bullet points. \n\n    Here's a step by step breakdown of what I expect from you. \n    ### INSTRUCTIONS ### \n    1) You will be provided with a series of bullet points and a job description. \n    2) You will view each text of a bullet point and then modify it based on the job description to secure a good ATS score. \n    3) Your output will be an unordered list of HTML elements, with each modified bullet point as an HTML list item.  \n    \n    NOTE: \n    * Your OUTPUT MUST be in HTML. \n    * Make sure to fix the spelling mistakes the job description and then generate the modified text. \n    * Make sure that the sentences are human-like. \n    * DO NOT HALLUCINATE\n    \n    ### JOB DESCRIPTION ###\n    ".concat(jobDescription, "\n\n    ### SECTION TEXT ### \n    ").concat(sectionContent, "\n\n    ### HTML UNORDERED LIST OUTPUT ###\n    ");
+    fetch(geminiApiUrl);
+    return "";
+}
+function addChangesToResume() {
+    var _a;
+    var jobDescription = document.getElementById("job-description").value;
+    var itemBodies = document.getElementsByClassName("item-body");
+    console.log(itemBodies);
+    for (var i = 0; i < itemBodies.length; i++) {
+        console.log((_a = itemBodies.item(i)) === null || _a === void 0 ? void 0 : _a.textContent);
+    }
+    // this function here will add links to resume almost immediately with raw javascript
+    addLinksToResume();
 }
--- index.ts
+++ index.ts
@@ -11,7 +11,7 @@ function wrapTextInHTML(url: string, textContent: string) : string {
     return isValidURL(url) && textContent ? `<a href="${url}">${textContent}</a>` : ""; 
 }  
 
-function insertContactInfo() : void {
+function addLinksToResume() : void {
     let huggingfaceUrl: string | null = (document.getElementById("huggingfaceUrl") as HTMLInputElement).value;  
     let githubUrl : string | null = (document.getElementById("githubUrl") as HTMLInputElement).value; 
     let linkedinUrl: string | null = (document.getElementById("linkedinUrl") as HTMLInputElement).value;  
@@ -41,5 +41,50 @@ function insertContactInfo() : void {
     } 
 
     (document.getElementById("contact-info") as HTMLElement).innerHTML = tagContent; 
-    console.log(tagContent); 
+} 
+
+function modifySectionsInResume(jobDescription: string, sectionContent: string): string {
+	let apiKey: string = (document.getElementById("api-key") as HTMLInputElement).value; 
+	const geminiApiUrl : string = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${apiKey}`
+
+    let prompt : string = `
+    You are a ATS-conqueror bot. Your goal is to modify a list of texts to beat the ATS algorithm and secure a good score. 
+    You will do this by using the a job description and then modifying the text I provide you with in bullet points. 
+
+    Here's a step by step breakdown of what I expect from you. 
+    ### INSTRUCTIONS ### 
+    1) You will be provided with a series of bullet points and a job description. 
+    2) You will view each text of a bullet point and then modify it based on the job description to secure a good ATS score. 
+    3) Your output will be an unordered list of HTML elements, with each modified bullet point as an HTML list item.  
+    
+    NOTE: 
+    * Your OUTPUT MUST be in HTML. 
+    * Make sure to fix the spelling mistakes the job description and then generate the modified text. 
+    * Make sure that the sentences are human-like. 
+    * DO NOT HALLUCINATE
+    
+    ### JOB DESCRIPTION ###
+    ${jobDescription}
+
+    ### SECTION TEXT ### 
+    ${sectionContent}
+
+    ### HTML UNORDERED LIST OUTPUT ###
+    `
+
+    fetch(geminiApiUrl)
+    return ""
+} 
+
+function addChangesToResume() : void {
+    let jobDescription: string = (document.getElementById("job-description") as HTMLTextAreaElement).value; 
+    let itemBodies : HTMLCollectionOf<Element> = document.getElementsByClassName("item-body"); 
+    console.log(itemBodies); 
+
+    for(let i = 0; i < itemBodies.length; i++) {
+        console.log(itemBodies.item(i)?.textContent) 
+    } 
+
+    // this function here will add links to resume almost immediately with raw javascript
+    addLinksToResume(); 
 }  
